f2bc36333b2d5b2ee63fb8e6b16ab1ba96feec1a
Anjalipradeep06 <anjalipradeep1214@gmail.com>
2026-06-21 16:37:21 +0530

    git api

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // 1. Create axios instance
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL + "api",
+  baseURL: process.env.REACT_APP_API_URL + "/api",
 });
 
 
