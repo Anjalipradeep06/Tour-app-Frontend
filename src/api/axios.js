1eba89bf2ab74aa694ab5a1be86d41e6460ac34b
Anjalipradeep06 <anjalipradeep1214@gmail.com>
2026-06-21 16:51:22 +0530

    api change

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,9 +2,8 @@ import axios from "axios";
 
 // 1. Create axios instance
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL + "/api",
+  baseURL: `${process.env.REACT_APP_API_URL}/api`,
 });
-
 
 
 api.interceptors.request.use(
